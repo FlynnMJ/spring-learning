9e991da610f8c345cef211efc6aad4ed0fb22399
18251938640@163.com <18251938640@163.com>
2018-11-23 17:02:53 +0800

    eureka replicas commit

---
+++ springcloud-learning/eureka-server/src/main/java/com/flynn/EurekaRegisterApplication.java
@@ -0,0 +1,14 @@
+package com.flynn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaRegisterApplication.class, args);
+    }
+}
